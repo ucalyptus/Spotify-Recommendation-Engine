aac9976d95287af30171ea02d400612933a73741
Rishabh Raizada <rishabh5102000@gmail.com>
2020-02-29 15:43:40 +0530

    Settings screen - added profile UI

--- spotify_mockup/lib/tabs/settings_tab.dart
+++ spotify_mockup/lib/tabs/settings_tab.dart
@@ -4,13 +4,81 @@ class SettingsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Theme.of(context).accentColor,
-      child: Center(
-        child: Text('Settings',
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 24)),
+      child: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+//              expandedHeight: MediaQuery.of(context).size.height / 10,
+              floating: true,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text("Settings",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      )),
+                  background: Container(
+                    color: Theme.of(context).primaryColor,
+                  )),
+            ),
+          ];
+        },
+        body: Container(
+          color: Theme.of(context).accentColor,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      child: Icon(
+                        Icons.account_circle,
+                        size: 80,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Rishabh Raizada",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              Text(
+                                "View Profile",
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          )),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                        size: 10,
+                      ),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
