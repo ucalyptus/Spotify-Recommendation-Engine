6bc53686c3cff9d7cad4857fbcc16e8c6cf63c9d
HimeshNayak <himeshnayak015@gmail.com>
2021-01-07 23:46:32 +0530

    Added lib files

---
+++ spotify_clone/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spotify Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
